a96dd2e53e8492bef86188d3db60d717d6e37103
Anne <anne725688@gmail.com>
2023-12-14 10:22:12 +0800

    變更alert

--- coupon(ZeroAnne)/doDeleteCoupon.php
+++ coupon(ZeroAnne)/doDeleteCoupon.php
@@ -12,16 +12,56 @@ $search = $_GET["search"];
 $sql = "UPDATE coupon SET coupon_valid='-1' WHERE id=$id";
 // echo $sql;
 // exit;
-
-if ($conn->query($sql) === TRUE) {
-    $_SESSION['message'] = "刪除成功";
-    header("Location: coupon-list.php?page=1&order=1");
-    exit();
-} else {
-    $_SESSION['message'] = "刪除失敗";
-    header("Location: coupon-list.php?page=1&order=1");
-    exit();
+if(isset($_GET["search"])){
+    $search=$_GET["search"];
+    if ($conn->query($sql) === TRUE) {
+        $_SESSION['message'] = "刪除成功";
+        header("Location: coupon-list.php?search=$search");
+        exit();
+    } else {
+        $_SESSION['message'] = "刪除失敗";
+        header("Location: coupon-list.php?search=$search");
+        exit();
+    }
+}elseif(isset($_GET["page"]) && isset($_GET["order"])){
+    $page=$_GET["page"];
+    $order=$_GET["order"];
+    if ($conn->query($sql) === TRUE) {
+        $_SESSION['message'] = "刪除成功";
+        header("Location: coupon-list.php?page=$page&order=$order");
+        exit();
+    } else {
+        $_SESSION['message'] = "刪除失敗";
+        header("Location: coupon-list.php?page=1&order=1");
+        exit();
+    }
+}elseif(isset($_GET["page"]) && isset($_GET["order"]) && isset($_GET["use"])){
+    if ($conn->query($sql) === TRUE) {
+        $page=$_GET["page"];
+        $use=$_GET["use"];
+        $order=$_GET["order"];
+        
+        $_SESSION['message'] = "刪除成功";
+        header("Location: coupon-list.php?page=$page&use=$use&order=$order");
+        exit();
+    } else {
+        $_SESSION['message'] = "刪除失敗";
+        header("Location: coupon-list.php?page=1&order=1");
+        exit();
+    }
 }
+else{
+    if ($conn->query($sql) === TRUE) {
+        $_SESSION['message'] = "刪除成功";
+        header("Location: coupon-list.php?page=1&order=1");
+        exit();
+    } else {
+        $_SESSION['message'] = "刪除失敗";
+        header("Location: coupon-list.php?page=1&order=1");
+        exit();
+    }
+}
+
 
 $conn->close();
 
